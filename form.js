2a6a2d15088f377dc4c44ef35ab3c23e40d1f8ea
Yash Mittal <yashmittal1819@gmail.com>
2020-10-31 12:03:45 +0530

    chat box

--- form.js
+++ form.js
@@ -1,7 +1,14 @@
 
 // Your web app's Firebase configuration
   var firebaseConfig = {
-   
+    apiKey: "AIzaSyBnFV1NPOLEBpg-3dDxr-CImPwBUOfXEsc",
+    authDomain: "connectiva-a6906.firebaseapp.com",
+    databaseURL: "https://connectiva-a6906.firebaseio.com",
+    projectId: "connectiva-a6906",
+    storageBucket: "connectiva-a6906.appspot.com",
+    messagingSenderId: "616492841512",
+    appId: "1:616492841512:web:9e88f3e99650d11b0b931d",
+    measurementId: "G-T8F0SPCM9Y"
     
   };
   // Initialize Firebase
